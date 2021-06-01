8d45ec9afbb9c7dbc2cf75f899bad6e61eb3cc8c
karan obhrai <mrkobhrai@gmail.com>
2021-06-01 23:20:05 +0100

    Eslint frontend

---
+++ react-frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  settings: {
+    react: {
+      "version": "detect"
+    }
+  },
+  env: {
+    "browser" : true,
+    "node" : true,
+    "es6" : true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "object-curly-spacing": ["error", "always"],
+    "no-undef": 2,
+    "no-unused-vars": 2
+  },
+  "parserOptions": {
+    "ecmaVersion": 11,
+    "sourceType": "module"
+  },
+};
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,6 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
     <div>
       Hello DRP 19!
