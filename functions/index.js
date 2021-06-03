846407cd528c1cc818cd8a3f51ba53d223581442
karan obhrai <mrkobhrai@gmail.com>
2021-06-03 11:50:13 +0100

    Ensure region hosting in london for backend

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions").region("europe-west2");
 const app = require("./app");
 
 exports.api = functions.https.onRequest(app);
