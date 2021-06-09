a5a1daed241ad99c8072623b2c80935d0d97e6a8
Jeewoo Kim <jeewoo1998@gmail.com>
2021-06-09 19:07:07 +0100

    Add test accounts json and code to authenticate test user accounts

--- functions/controllers/login.js
+++ functions/controllers/login.js
@@ -2,9 +2,30 @@ const express = require("express");
 const loginRouter = new express.Router();
 const authImperial = require("auth-imperial");
 
+const testAccounts = [
+  {
+    username: "demo1",
+    password: "imperialDRP1",
+  },
+  {
+    username: "demo2",
+    password: "imperialDRP2",
+  },
+];
+
 loginRouter.post("/", async (req, res, next) => {
   const shortcode = req.body.shortcode;
   const password = req.body.password;
+
+  // Check if it matches testAccounts
+  const testAccount = testAccounts.filter((testAccount) =>
+    testAccount.username === shortcode);
+  const isPasswordCorrect = testAccount.password === password;
+
+  if (testAccount !== null) {
+    return res.json({ isPasswordCorrect, shortcode });
+  }
+
   // invalid params: no shortcode or password
   if (shortcode === undefined || password === undefined) {
     return next({
