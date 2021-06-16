4eec42b0ae09dc4b6736a36fe1f87693e726701a
karan obhrai <mrkobhrai@gmail.com>
2021-06-16 03:01:31 +0100

    Add map page linking

--- react-frontend/src/views/AreaMapPage.js
+++ react-frontend/src/views/AreaMapPage.js
@@ -8,7 +8,7 @@ import ErrorBox from "../components/ErrorBox";
 import { createListener } from "../utils/EventListeners";
 import { MapInteractionCSS } from "react-map-interaction";
 
-const SeatMarker = ({ location, isBooked }) => {
+const SeatMarker = ({ location, isBooked, code, setSelectedCode }) => {
   const seatStyle = {
     width: "25vw",
     height: "25vw",
@@ -20,12 +20,22 @@ const SeatMarker = ({ location, isBooked }) => {
     left: location.x,
   };
 
-  return <div style={seatStyle} />;
+  const setCode = () => {
+    setSelectedCode(code);
+  };
+
+  return <div style={seatStyle} onTouchStart={setCode} onClick={setCode}/>;
 };
 
-const SeatMarkers = ({ areaInfo }) =>
+const SeatMarkers = ({ areaInfo, setSelectedCode }) =>
   areaInfo.seats.map(({ seatId, location, isBooked }) => (
-    <SeatMarker key={seatId} location={location} isBooked={isBooked} />
+    <SeatMarker
+      key={seatId}
+      location={location}
+      isBooked={isBooked}
+      code={seatId}
+      setSelectedCode={setSelectedCode}
+    />
   ));
 
 const AreaMapPage = ({ user }) => {
@@ -41,6 +51,8 @@ const AreaMapPage = ({ user }) => {
   const [error, setError] = useState("");
 
   const [currBooking, setCurrBooking] = useState("");
+
+  const [selectedCode, setSelectedCode] = useState("");
 
   const paddedDivStyle = {
     margin: "10vh 10vw",
@@ -85,7 +97,19 @@ const AreaMapPage = ({ user }) => {
     overflow: "scroll",
   };
 
-  const borderMapStyle = { height: "60vh", border: "2px solid black" };
+  const codeBoxStyle = {
+    fontSize: "5vw",
+    width: "79vw",
+    border: "1px solid black",
+    padding: "2vw 0",
+    margin: "auto",
+    textAlign: "center",
+  };
+
+  const borderMapStyle = {
+    height: "60vh",
+    border: "2px solid black",
+  };
 
   return (
     <div style={paddedDivStyle}>
@@ -96,9 +120,15 @@ const AreaMapPage = ({ user }) => {
         <MapInteractionCSS style={mapDivStyle}>
           <div>
             <img src={areaInfo.map} alt="Area Map" />
-            <SeatMarkers areaInfo={areaInfo} />
+            <SeatMarkers
+              areaInfo={areaInfo}
+              setSelectedCode={setSelectedCode}
+            />
           </div>
         </MapInteractionCSS>
+      </div>
+      <div style={codeBoxStyle}>
+        <b>Code</b>: {selectedCode}{" "}
       </div>
       <CodeOrStatusButton seatId={currBooking} />
     </div>
