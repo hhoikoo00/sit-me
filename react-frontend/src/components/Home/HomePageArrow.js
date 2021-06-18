84cf05d0c9a2644787a5553483e79aef8bd7995e
karan obhrai <mrkobhrai@gmail.com>
2021-06-18 12:05:08 +0100

    Update back arrow in line with copyright licenses

--- react-frontend/src/components/Home/HomePageArrow.js
+++ react-frontend/src/components/Home/HomePageArrow.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import returnArrow from "../../assets/icons/backArrow.png";
+import returnArrow from "../../assets/icons/backArrow.svg";
 
 const HomePageArrow = ({ margin }) => {
   return (
